39cce8770644240529bda794c4e25cf5271d09d1
shu-chou <jchoudhary1994@gmail.com>
2020-02-16 19:14:11 +0530

    Remove keys

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,14 +1,17 @@
 const request = require('request');
 const geocode = (placename, callback) => {
     setTimeout(() => {
-       const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(placename)+'.json?access_token=pk.eyJ1Ijoic2h1LWNobyIsImEiOiJjazZodnJtOWMyeWs0M2xxamYybHFnNDluIn0.MaxNJjlUbR4n8XbJo3YTOw';
+       const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(placename)+'.json?access_token=MaxNJjlUbR4n8XbJo3YTOw';
        request({ url: url, json: true }, (error, response) => {
            if (error){
                callback({ error: 'Error' }, undefined)
            }else{
                try {
-                   callback(undefined, [response.body.features[0].center[1],response.body.features[0].center[0], response.body.features[0].place_name])
-                   console.log();
+                   callback(undefined, {
+                       lat: response.body.features[0].center[1],
+                       long: response.body.features[0].center[0],
+                       place: response.body.features[0].place_name
+                   });
                } catch (e) {
                    callback({ error: 'Couldnt find what you were looking for' }, undefined);
                }
